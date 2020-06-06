9884927c2a01fd754fda6f30af06953729981236
sven-gerlach <svengerlach@me.com>
2020-06-06 23:56:27 +0100

    creating a pygame window and responding to user input

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,28 @@
-"""old arcade game alien invasion"""
+"""Arcade Game: Alien Invasion"""
+import sys
+import pygame
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+    def __init__(self):
+        """Initialise the game, and create game resources."""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1440, 900))
+        pygame.display.set_caption("Alien Invasion", 'AI')
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            # watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # make the most recently drawn screen visible.
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
