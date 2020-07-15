c7b5c1aa4d7fc774ea2d939f7e49b3281fc93356
sven-gerlach <svengerlach@me.com>
2020-07-15 17:22:49 +0100

    Refactoring

--- settings.py
+++ settings.py
@@ -7,3 +7,19 @@ class Settings:
         self.screen_width = 1430
         self.screen_height = 880
         self.bg_colour = (230, 230, 230)
+
+        # ship settings
+        self.ship_speed = 1.5
+
+        # bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        # alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
--- ship.py
+++ ship.py
@@ -6,14 +6,32 @@ class Ship:
     def __init__(self, ai_game):
         """ Initialise the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship.bmp').convert()
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
+        # movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update the ship's x value, not the rect"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
